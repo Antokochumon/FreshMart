1d67947a08e94584b2b39f5ea94599e25fae8206
Antokochumon <Antokochumon2@gmail.com>
2025-05-10 11:31:11 +0100

    Add files via upload

---
+++ firebase/config.js
@@ -0,0 +1,13 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDU6li87RVbo5uHsQkM5cVpiT16Cjp4GwY",
+  authDomain: "anto-343cf.firebaseapp.com",
+  projectId: "anto-343cf",
+  storageBucket: "anto-343cf.firebasestorage.app",
+  messagingSenderId: "101895361272",
+  appId: "1:101895361272:web:5599e4de733055b92e610d",
+  measurementId: "G-YR2J7DEXRD"
+};
+
+// ✅ This line is required
+firebase.initializeApp(firebaseConfig);
